ed7eba361922e52a9db6a2440eddad3df729c5b4
haeryny <haerynjack@gmail.com>
2022-11-01 13:39:13 -0700

    haeryn

--- main.py
+++ main.py
@@ -25,16 +25,9 @@ def stub():
 def sophia():
     return render_template("sophia.html")
 
-
-@app.route('/liav/')  # connects /stub/ URL to stub() function
-def liav():
-    return render_template("liav.html")
-
-
 @app.route('/haeryn/')  # connects /stub/ URL to stub() function
 def haeryn():
     return render_template("haeryn.html")
-
 
 @app.route('/annika/')  # connects /stub/ URL to stub() function
 def annika():
