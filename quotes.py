f722918b0c4b59da8f5a7b55e64053d38663ee57
sophia926 <sophiahytang@gmail.com>
2022-11-01 20:16:57 -0700

    more api

--- quotes.py
+++ quotes.py
@@ -4,26 +4,26 @@ from flask_restful import Api, Resource # used for REST API building
 import requests  # used for testing 
 
 # Blueprints enable python code to be organized in multiple files and directories https://flask.palletsprojects.com/en/2.2.x/blueprints/
-gratitude_api = Blueprint('gratitude_api', __name__,
-                   url_prefix='/api/gratitude')
+quotes_api = Blueprint('quotes_api', __name__,
+                   url_prefix='/api/quotes')
 
 # API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
-api = Api(gratitude_api)
+api = Api(quotes_api)
 
 
 
 def getAPI():
 
 
-    url = "https://gratitude-questions.p.rapidapi.com/question"
+    url = "https://inspiring-quotes.p.rapidapi.com/random"
 
     headers = {
         "X-RapidAPI-Key": "222daebd02msh197e0db183672fap17cc9djsn8a1f2f813e5b",
-        "X-RapidAPI-Host": "gratitude-questions.p.rapidapi.com"
+        "X-RapidAPI-Host": "inspiring-quotes.p.rapidapi.com"
     }
 
     response = requests.request("GET", url, headers=headers)
-    gratitude_data = response
+    quote_data = response
     return response
 
 
@@ -31,5 +31,5 @@ if __name__ == "__main__":
 
     # This code looks for "world data"
     response = getAPI()
-    print("World Totals")
-    question = response.json().get('question')
+    quote = response.json().get('quote')
+    person = response.json().get('author')
